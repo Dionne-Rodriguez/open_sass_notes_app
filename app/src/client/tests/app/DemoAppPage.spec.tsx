67c5e044efb47f59dbc5baef14579949e09c3444
Dionne <dionne.r.9@gmail.com>
2024-03-12 22:23:18 -0400

    initial test setup & revert commit

---
+++ app/src/client/tests/app/DemoAppPage.spec.tsx
@@ -0,0 +1,44 @@
+import { mockServer, renderInContext } from 'wasp/client/test';
+import { test, expect } from 'vitest';
+import { screen } from '@testing-library/react';
+import { getAllTasksByUser } from 'wasp/client/operations';
+import DemoApp from '../../app/DemoAppPage';
+
+const { mockQuery } = mockServer();
+
+const mockTasks = [
+  {
+    id: '1',
+    description: 'test todo 1',
+    isDone: true,
+    userId: 1,
+    createdAt: new Date(),
+    time: 'test-time',
+  },
+  //   {
+  //     id: '2',
+  //     description: 'test todo 2',
+  //     isDone: false,
+  //     userId: 1,
+  //     createdAt: new Date(),
+  //     time: 'test-time',
+  //   },
+];
+
+test('renders all the tasks', async () => {
+  mockQuery(getAllTasksByUser, mockTasks);
+
+  renderInContext(<DemoApp />);
+
+  //const checkBoxes = await screen.getAllByRole('checkbox');
+
+  await screen.findByText('test todo 1');
+
+  expect(screen.getByRole('checkbox')).toBeChecked();
+
+  //   await screen.findByText('test todo 2');
+
+  //   expect(screen.getByRole('checkbox')).not.toBeChecked();
+
+  screen.debug();
+});
